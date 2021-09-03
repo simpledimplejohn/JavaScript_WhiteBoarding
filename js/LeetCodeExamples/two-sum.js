d8d4c447f4f8946ec2a5fe51316c45b400c737d6
Simple Dimple John <simpledimplejohn@gmail.com>
2021-09-03 08:11:53 -0700

    add question text

--- js/LeetCodeExamples/two-sum.js
+++ js/LeetCodeExamples/two-sum.js
@@ -18,14 +18,12 @@ var twoSum = function(nums, target) {
   for(let i = 0; i < nums.length; i ++) {
 
     for(let j = 0; j < nums.length; j ++) {
-      
-
+      console.log(nums[i], " + ", nums[j])
       if(nums[i] + nums[j] === target) {
         number.push(i, j)
 
         
       }
-      console.log(nums[i], " + ", nums[j])
     }
   }
 
